c6b40dac1d9e202e2d006f4e23bfc0ea9de6d02e
haubaustin <haubaustin@gmail.com>
2022-04-19 16:47:11 -0700

    CSS

--- src/components/PostArticle.jsx
+++ src/components/PostArticle.jsx
@@ -45,7 +45,8 @@ const PostArticle = ({user}) => {
                 <input type = 'text' name='image' placeholder = 'Image Link' className = 'articleTitle' onChange={handleChange} value={blog.image}/>
                 <br />
                 <br />
-                <input type = 'text' name='article' placeholder = 'Content' className = 'articleContent' onChange={handleChange} value={blog.article}/>
+                <textarea name='article' placeholder='Type Your Article Here' className='articleTextArea' onChange={handleChange} value={blog.article} />
+                {/* <input type = 'text' name='article' placeholder = 'Content' className = 'articleContent' onChange={handleChange} value={blog.article}/> */}
                 <br />
                 <br />
                 <div className = 'articlePostButton'>
