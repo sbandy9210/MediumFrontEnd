43755d509ee5f8b27a1956a26bdef6bcc1a72d78
haubaustin <haubaustin@gmail.com>
2022-04-18 18:06:34 -0700

    Added API to MyPage. Mapped COmponents

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,25 +1,37 @@
 import PostArticle from '../components/PostArticle'
+import Client from '../services/api'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const MyPage = (props) => {
+    const [data, setData] = useState([])
+
+useEffect(() => {
+    const blogs = async () => {
+        const myBlogs = await Client.get(`/api/blog/author/${props.user.id}`)
+        console.log(myBlogs.data)
+        setData(myBlogs.data)
+    }
+    blogs()
+}, [])
+
+   
+
     return(
         <div>
-            <div className = 'myPageLeftDiv'>
-                <div>
-                    <h3>Title 1</h3>
+            <div className = 'myBlogs'>
+            {data && data.map((dat) => (
+                <div key={dat.id}>
+                    <Link to={`/blog/${dat.id}`}>
+                    <h3>{dat.title}</h3>
+                    </Link>
                     <img 
-                        src = 'https://www.dankultura.org/wp-content/uploads/2015/10/Article-Writing-Can-be-a-Sure-Wager.jpg'
+                        src ={dat.image}
                         alt = ''
                         style = {{width: '150px'}}
                     />
                 </div>
-                <div>
-                    <h3>Title 2</h3>
-                    <img 
-                        src = 'https://www.dankultura.org/wp-content/uploads/2015/10/Article-Writing-Can-be-a-Sure-Wager.jpg'
-                        alt = ''
-                        style = {{width: '150px'}}
-                    />
-                </div>
+            ))}
             </div>
             <div className = 'myPageRightDiv'>
                 <PostArticle user={props.user}/>
