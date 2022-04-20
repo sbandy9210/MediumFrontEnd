78394f0449bc7791c07d31291e57af2204ea04da
Piero Caceres <pierocaceres92@yahoo.com>
2022-04-20 11:00:09 -0400

    Fixed indentation. Added user/authentication to MyPage

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,10 @@
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { SignInUser } from '../services/Auth'
 import { useNavigate } from 'react-router-dom'
 
 
 
 const SignIn = (props) => {
-    // const { setUser, setAuthenticated } = useContext(DataContext)
     let navigate = useNavigate()
 
     const [formValues, setFormValues] = useState({
@@ -34,41 +33,37 @@ const SignIn = (props) => {
     return(
         <div className='SignIn'>
             <div>
-            <h1>Sign in.</h1>
-    
-            <form className='signin' onSubmit={handleSubmit}>
-            
-            <div className='signInUsername'>
-            <input 
-                type='text' 
-                id='username' 
-                placeholder='Enter your username' 
-                name='username' 
-                onChange={handleChange} 
-                value={formValues.username}
-                style={{ textAlign: 'center'}}
-             /> <br/>
-             </div>
-            
-
-            <div className='signInPassword'>
-            <input 
-                type='password' 
-                id='password' 
-                placeholder='Enter your password' 
-                name='password' 
-                onChange={handleChange} 
-                value={formValues.password}
-                style={{ textAlign: 'center'}}
-            /> <br/>
-            </div>
-
-            <button 
-            className='signInButton'
-            type='submit'
-            disabled={!formValues.username || !formValues.password}> Sign In </button>
-
-            </form>
+                <h1>Sign in.</h1>
+                <form className='signin' onSubmit={handleSubmit}>
+                    <div className='signInUsername'>
+                    <input 
+                        type='text' 
+                        id='username' 
+                        placeholder='Enter your username' 
+                        name='username' 
+                        onChange={handleChange} 
+                        value={formValues.username}
+                        style={{ textAlign: 'center'}}
+                    /> 
+                    <br/>
+                    </div>
+                    <div className='signInPassword'>
+                    <input 
+                        type='password' 
+                        id='password' 
+                        placeholder='Enter your password' 
+                        name='password' 
+                        onChange={handleChange} 
+                        value={formValues.password}
+                        style={{ textAlign: 'center'}}
+                    /> 
+                    <br/>
+                    </div>
+                    <button 
+                    className='signInButton'
+                    type='submit'
+                    disabled={!formValues.username || !formValues.password}> Sign In </button>
+                </form>
             </div>
         </div>
     )
