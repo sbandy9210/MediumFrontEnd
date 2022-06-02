cd7d2bef25aa7599f078423aef1567b21a10909d
Piero Caceres <pierocaceres92@yahoo.com>
2022-06-02 13:58:25 -0400

    Enabled deployed backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
-// export const BASE_URL = 'https://mylieu-backend.herokuapp.com/'
+// export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'https://mylieu-backend.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
