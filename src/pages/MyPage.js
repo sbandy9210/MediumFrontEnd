de40cfebc97ec218e53c121ac79199f0d02254a1
Piero Caceres <pierocaceres92@yahoo.com>
2022-06-02 16:09:15 -0400

    Updated display grid for MyPage. Conditionally render user blog or author blog.

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -10,21 +10,18 @@ const MyPage = ({ user, authenticated }) => {
     const { user_id } = useParams()
 
     const [data, setData] = useState([])
+    const [subscribedT0, setSubscribedTo] = useState([])
+    const [myBlog, setMyBlog] = useState(true)
 
     const blogs = async () => {
         const myBlogs = await Client.get(`/api/blog/author/${user_id}`)
         setData(myBlogs.data)
     }
-    
-    useEffect(() => {
-        blogs()
-    }, [])
 
-    return (user && authenticated ) ? (
-        (data.length > 0) ? (
-            <div className='MyPage'>
-                <div className = 'myBlogs'>
-                    <h2 className='myBlogsH2'>My Blogs</h2>
+    const showMyBlog = () => {
+        return (
+           <div className='container'>
+               <h2 className='myBlogsH2'>My Blogs</h2>
                 {data && data.map((dat) => (
                     <div className='myBlogTitle' key={dat.id}>
                         <Link to={`/${user_id}/blog/${dat.id}`} className="navLink">
@@ -32,6 +29,48 @@ const MyPage = ({ user, authenticated }) => {
                         </Link>
                     </div>
                 ))}
+           </div> 
+        )
+    }
+
+    const showSubscrtibeTo = () => {
+        return (
+            <div className='container'>
+                <h2 className='myBlogsH2'>{} Blogs</h2>
+                 {subscribedT0 && subscribedT0.map((data) => (
+                     <div className='myBlogTitle' key={data.id}>
+                         <Link to={`/${user_id}/blog/${data.id}`} className="navLink">
+                             
+                         </Link>
+                     </div>
+                 ))}
+            </div> 
+         )
+    }
+
+    const displaySubcribedTo = async () => {
+        const subscribed = await Client.get(`api/following/${user_id}`)
+        console.log(subscribed.data.Following)
+        setSubscribedTo(subscribed.data.Following)
+    }
+    
+    useEffect(() => {
+        blogs()
+        displaySubcribedTo()
+    }, [])
+
+    return (user && authenticated ) ? (
+        (data.length > 0) ? (
+            <div className='ParentDiv'>
+                <div className='MyPage'>
+                    <div className = 'myBlogs'>
+                        {myBlog && showMyBlog()}
+                        {!myBlog && showSubscrtibeTo()}
+                    </div>
+                </div>
+                <div className='SubscribedTo'>
+                    <h2 className='myBlogsH2'>Subscribed to:</h2>
+                    {/* {showSubscrtibeTo()} */}
                 </div>
             </div>
         ) : (
